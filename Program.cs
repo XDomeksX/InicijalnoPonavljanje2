e5a1186d9c6adaa329f712a381c9b30c97746731
Domenik Milohanić <90246598+XDomeksX@users.noreply.github.com>
2022-09-12 12:35:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicialnoPonavljanje2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            start: string input = Console.ReadLine();
+            bool c = char.IsWhiteSpace(input, counter);
+            if (c == true)
+            {
+                Console.WriteLine("_");
+            }
+            else if (c == false)
+            {
+                Console.WriteLine(input);
+            }
+            goto start;
+            Console.ReadKey();
+        }
+    }
+}
